866e392312dd925d0dd9fe7fa436def67c265186
Josh English <josh@joshenglish.com>
2016-04-08 16:00:01 -0500

    Created Routes and added Handlebars to former index.html that is now default.hbs

---
+++ app.js
@@ -0,0 +1,26 @@
+// Require our dependencies
+// ------------------------
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    app     = express();
+
+// Configure settings
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.engine('hbs', exphbs({
+  defaultLayout: 'default',
+  extname:       '.hbs'
+}));
+
+app.set('view engine', 'hbs'); // matching the engine
+
+
+app.get('/', function(req, res) {
+  res.render('home', { title: 'take your work to the next level' });
+});
+
+// Start a server
+// --------------
+var server = app.listen(3000, function() {
+  console.log('The server is up and running on port ' + server.address().port + '.');
+});
